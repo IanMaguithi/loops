7a8fb8c55833fddbc2809ca8b2d524ded3dc9cb0
IanMaguithi <maguithian13427@gmail.com>
2023-05-27 14:13:36 +0300

    returning values from loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    print_result();
+}
+
+// returning values from loops
+fn print_result(){
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
 }
