ffb654850a6a54d3f84cf69ff85e0e735b4b5f67
IanMaguithi <maguithian13427@gmail.com>
2023-05-27 14:33:12 +0300

    differentiate loops with labels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     print_result();
+    print_result_with_label();
 }
 
 // returning values from loops
@@ -15,4 +16,27 @@ fn print_result(){
     };
 
     println!("The result is {}", result);
+}
+
+// loop labels to disambiguate between multiple loops
+fn print_result_with_label(){
+    let mut count = 0;
+    'counting_up: loop{
+        println!("count = {count}");
+        let mut remaining =10;
+
+        'counting_down: loop{
+            println!("remaining = {}", remaining);
+            // if this wasn't labelled, it would break the inner loop
+            if remaining == 9{
+                break 'counting_down;
+            }
+            if count == 2{
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {}", count);
 }
